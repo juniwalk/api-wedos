a7f198a22ab0354e62b39e2f9a28e553836974f2
juniwalk <juniwalk@outlook.cz>
2022-05-06 15:05:05 +0200

    Added account-list method

--- src/Subsystems/DomainSubsystem.php
+++ src/Subsystems/DomainSubsystem.php
@@ -13,6 +13,9 @@ use Nette\Schema\Expect;
 trait DomainSubsystem
 {
 	/**
+	 * @param  string  $name
+	 * @param  string[]  $params
+	 * @return Response
 	 * @see https://kb.wedos.com/en/wapi-api-interface/wapi-command-domain-info/
 	 */
 	public function domainInfo(string $name, iterable $params = []): Response
--- src/Subsystems/VariousSubsystem.php
+++ src/Subsystems/VariousSubsystem.php
@@ -7,6 +7,7 @@
 
 namespace JuniWalk\Wedos\Subsystems;
 
+use DateTime;
 use JuniWalk\Wedos\Response;
 use Nette\Schema\Expect;
 
@@ -22,10 +23,30 @@ trait VariousSubsystem
 
 
 	/**
+	 * @return Response
 	 * @see https://kb.wedos.com/en/wapi-api-interface/wapi-command-credit-info/
 	 */
 	public function creditInfo(): Response
 	{
 		return $this->call('credit-info');
+	}
+
+
+	/**
+	 * @param  DateTime  $dateFrom
+	 * @param  DateTime  $dateTo
+	 * @return Response
+	 * @see https://kb.wedos.com/en/wapi-api-interface/wapi-account-list/
+	 */
+	public function accountList(DateTime $dateFrom, DateTime $dateTo): Response
+	{
+		$params['date_from'] = $dateFrom->format('Y-m-d');
+		$params['date_to'] = $dateTo->format('Y-m-d');
+		$params = $this->check($params, [
+			'date_from'	=> Expect::string()->required(),
+			'date_to'	=> Expect::string()->required(),
+		]);
+
+		return $this->call('account-list', '', $params);
 	}
 }
